b07a3af71a5e4b5262590c9e2f93907b9c9ed4e4
The Mighty Websters <36897054+themightywebsters@users.noreply.github.com>
2018-03-04 01:04:09 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(window).load(function(){
+10$(window).load(function(){
      $('.preloader').fadeOut('slow');
 });
 
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "March 8, 2018 00:00:00",
+	date: "March 10, 2018 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
